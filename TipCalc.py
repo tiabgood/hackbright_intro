0b14e4d26537e38d6aeaa033f718444900a41f2e
Tia Barnard <tbarnard@salesforce.com>
2015-09-01 09:55:17 -0700

    New TipCalc

---
+++ TipCalc.py
@@ -0,0 +1,6 @@
+print "Hackbright Tip Calculator"
+bill = float( raw_input ("How much was your bill? "))
+print "15 percent tip is $%.2f " %(bill * 0.15)
+print "20 percent tip is $%.2f " %(bill * 0.20)
+print "20 percent tip is $%.2f " %(bill * 0.25)
+
