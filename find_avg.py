8b17063a53fbf9268b901bbaee5931a75c75b842
Tia Barnard <tbarnard@salesforce.com>
2015-09-01 09:55:56 -0700

    New File

---
+++ find_avg.py
@@ -0,0 +1,19 @@
+def find_average (num1, num2, num3):
+    if type(num1)==int or type(num1)==float: 
+        if type(num2)==int or type(num2)==float:
+            if type(num3)==int or type(num3)==float:
+        	average = float((num1+num2+num3)/3)
+    	        return "Average is", average 
+	    else:
+	        return "Sorry the input(s) are invalid"
+    else:
+    	return "Sorry the input(s) are invalid"
+
+
+print find_average(3,4,4)
+
+print find_average(1,2,3)
+
+print find_average('6', 8, 7)
+
+print find_average(2, 9,'hi')
