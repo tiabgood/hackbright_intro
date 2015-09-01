9a020b2f2681e6e64724491a28afbbe6811a7253
Tia Barnard <tbarnard@salesforce.com>
2015-09-01 11:43:29 -0700

    Created a cleaner more thourogh code.

--- find_avg.py
+++ find_avg.py
@@ -1,13 +1,9 @@
 def find_average (num1, num2, num3):
-    if type(num1)==int or type(num1)==float: 
-        if type(num2)==int or type(num2)==float:
-            if type(num3)==int or type(num3)==float:
-        	average = float((num1+num2+num3)/3)
-    	        return "Average is", average 
-	    else:
-	        return "Sorry the input(s) are invalid"
+    if (type(num1)==int or type(num1)==float) and (type(num2)==int or type(num2)==float) and (type(num3)==int or type(num3)==float):
+            average = float((num1+num2+num3)/3)
+            return "Average is", average
     else:
-    	return "Sorry the input(s) are invalid"
+        return "Sorry the input(s) are invalid"
 
 
 print find_average(3,4,4)
@@ -17,3 +13,5 @@ print find_average(1,2,3)
 print find_average('6', 8, 7)
 
 print find_average(2, 9,'hi')
+
+print find_average(2, '9','hi')
